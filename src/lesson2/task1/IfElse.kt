5c20afbe0314c287cad2656333fc516cf3f03d1b
LGodrick <godrickrus@gmail. com>
2019-09-26 13:50:54 +0300

    выполнено все [2]

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -3,7 +3,11 @@
 package lesson2.task1
 
 import lesson1.task1.discriminant
+import lesson1.task1.sqr
+import javax.tools.Diagnostic
+import kotlin.math.abs
 import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.sqrt
 
 /**
@@ -76,66 +80,90 @@ fun ageDescription(age: Int): String {
     return "$age $answ"
 }
 
-    /**
-     * Простая
-     *
-     * Путник двигался t1 часов со скоростью v1 км/час, затем t2 часов — со скоростью v2 км/час
-     * и t3 часов — со скоростью v3 км/час.
-     * Определить, за какое время он одолел первую половину пути?
-     */
+/**
+ * Простая
+ *
+ * Нa шахматной доске стоят черный король и две белые ладьи (ладья бьет по горизонтали и вертикали).
+ * Определить, не находится ли король под боем, а если есть угроза, то от кого именно.
+ * Вернуть 0, если угрозы нет, 1, если угроза только от первой ладьи, 2, если только от второй ладьи,
+ * и 3, если угроза от обеих ладей.
+ * Считать, что ладьи не могут загораживать друг друга
+ */
+fun whichRookThreatens(kingX: Int, kingY: Int, rookX1: Int, rookY1: Int, rookX2: Int, rookY2: Int): Int {
+    return when {
+        ((kingX - rookX1 == 0 || kingY - rookY1 == 0) && (kingX - rookX2 == 0 || kingY - rookY2 == 0)) -> 3
+        ((kingX - rookX1 != 0 || kingY - rookY1 != 0) && (kingX - rookX2 == 0 || kingY - rookY2 == 0)) -> 2
+        ((kingX - rookX1 == 0 || kingY - rookY1 == 0) && (kingX - rookX2 != 0 || kingY - rookY2 != 0)) -> 1
+        else -> 0
+    }
+}
+
+
+/**
+ * Простая
+ *
+ * Путник двигался t1 часов со скоростью v1 км/час, затем t2 часов — со скоростью v2 км/час
+ * и t3 часов — со скоростью v3 км/час.
+ * Определить, за какое время он одолел первую половину пути?
+ */
 fun timeForHalfWay(
     t1: Double, v1: Double,
     t2: Double, v2: Double,
     t3: Double, v3: Double
-): Double = TODO()
+): Double {
+    val S1: Double = t1 * v1
+    val S2: Double = t2 * v2
+    val S3: Double = t3 * v3
+    val halfS = (S1 + S2 + S3) / 2
+    val answ: Double
+    answ = when {
+        halfS - S1 <= 0 -> halfS / v1
+        halfS - S2 - S1 <= 0 -> t1 + (halfS - S1) / v2
+        else -> t1 + t2 + (halfS - S1 - S2) / v3
+    }
+    return answ
+}
 
-    /**
-     * Простая
-     *
-     * Нa шахматной доске стоят черный король и две белые ладьи (ладья бьет по горизонтали и вертикали).
-     * Определить, не находится ли король под боем, а если есть угроза, то от кого именно.
-     * Вернуть 0, если угрозы нет, 1, если угроза только от первой ладьи, 2, если только от второй ладьи,
-     * и 3, если угроза от обеих ладей.
-     * Считать, что ладьи не могут загораживать друг друга
-     */
-    fun whichRookThreatens(
-        kingX: Int, kingY: Int,
-        rookX1: Int, rookY1: Int,
-        rookX2: Int, rookY2: Int
-    ): Int = TODO()
 
-    /**
-     * Простая
-     *
-     * На шахматной доске стоят черный король и белые ладья и слон
-     * (ладья бьет по горизонтали и вертикали, слон — по диагоналям).
-     * Проверить, есть ли угроза королю и если есть, то от кого именно.
-     * Вернуть 0, если угрозы нет, 1, если угроза только от ладьи, 2, если только от слона,
-     * и 3, если угроза есть и от ладьи и от слона.
-     * Считать, что ладья и слон не могут загораживать друг друга.
-     */
-    fun rookOrBishopThreatens(
-        kingX: Int, kingY: Int,
-        rookX: Int, rookY: Int,
-        bishopX: Int, bishopY: Int
-    ): Int = TODO()
+fun rookOrBishopThreatens(kingX: Int, kingY: Int, rookX: Int, rookY: Int, bishopX: Int, bishopY: Int): Int = when {
+    (kingX == rookX || kingY == rookY) && (abs(kingX - bishopX) == abs(kingY - bishopY)) -> 3
+    (kingX == rookX || kingY == rookY) -> 1
+    (abs(kingX - bishopX) == abs(kingY - bishopY)) -> 2
+    else -> 0
+}
 
-    /**
-     * Простая
-     *
-     * Треугольник задан длинами своих сторон a, b, c.
-     * Проверить, является ли данный треугольник остроугольным (вернуть 0),
-     * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
-     * Если такой треугольник не существует, вернуть -1.
-     */
-    fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
+/**
+ * Простая
+ *
+ * Треугольник задан длинАми своих сторон a, b, c.
+ * Проверить, является ли данный треугольник остроугольным (вернуть 0),
+ * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
+ * Если такой треугольник не существует, вернуть -1.
+ */
+fun triangleKind(a: Double, b: Double, c: Double): Int {
+    val a1 = sqr(a)
+    val b1 = sqr(b)
+    val c1 = sqr(c)
+    return when {
+        (a1 < b1 + c1) && (b1 < a1 + c1) && (c1 < b1 + a1) -> 0
+        (a1 == b1 + c1) || (b1 == a1 + c1) || (c1 == b1 + a1) -> 1
+        (a + b > c) && (a + c > b) && (c + b > a) -> 2
+        else -> -1
+    }
+}
 
-    /**
-     * Средняя
-     *
-     * Даны четыре точки на одной прямой: A, B, C и D.
-     * Координаты точек a, b, c, d соответственно, b >= a, d >= c.
-     * Найти длину пересечения отрезков AB и CD.
-     * Если пересечения нет, вернуть -1.
-     */
-    fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+/**
+ * Средняя
+ *
+ * Даны четыре точки на одной прямой: A, B, C и D.
+ * Координаты точек a, b, c, d соответственно, b >= a, d >= c.
+ * Найти длину пересечения отрезков AB и CD.
+ * Если пересечения нет, вернуть -1.
+ */
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
+    val answ = min(b, d) - max(a, c)
+    return if (answ > -1) answ
+    else -1
+}
+
+
--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -3,6 +3,7 @@
 package lesson2.task2
 
 import lesson1.task1.sqr
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -18,7 +19,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean = number / 1000 + (number / 100 % 10) == number % 10 + ((number % 100) / 10)
 
 /**
  * Простая
@@ -27,7 +28,7 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = (x1 + y1 == x2 + y2) || (x1 == x2) || (y1 == y2)
 
 
 /**
@@ -36,7 +37,23 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    return when {
+        month == 2 && ((year % 4 != 0) || ((year % 100 == 0) && (year % 400 != 0))) -> 28
+        month == 1 -> 31
+        month == 3 -> 31
+        month == 4 -> 30
+        month == 5 -> 31
+        month == 6 -> 30
+        month == 7 -> 31
+        month == 8 -> 31
+        month == 9 -> 30
+        month == 10 -> 31
+        month == 11 -> 30
+        month == 12 -> 31
+        else -> 29
+    }
+}
 
 /**
  * Средняя
@@ -48,7 +65,7 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean = r2 >= (sqrt(sqr(x1 - x2) + sqr(y1 - y2)) + r1)
 
 /**
  * Средняя
@@ -59,4 +76,7 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int):
+        Boolean = (((a <= r) || (a <= s)) && ((b <= r) || (b <= r)))
+        || (((b <= r) || (b <= s)) && ((c <= r) || (c <= r)))
+        || (((a <= r) || (a <= s)) && ((c <= r) || (c <= r)))
